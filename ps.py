ec901b3a9d086ce4ead70ce8dceba258516bb5c9
srinivas peddada <srinivas23peddada@gmail.com>
2025-05-19 12:43:05 +0530

    19task is added

---
+++ ps.py
@@ -0,0 +1,33 @@
+# 1. Print numbers from 1 to 10 using a loop
+for i in range(1,11):
+    print(i)
+print()
+
+
+# 2. Print all even numbers between 1 and 20
+print("even numbers")
+for i in range(2,21,2):
+    print(i)
+print()
+
+# 3. Print the characters of the string "+" python "+" one by one using a loop
+print("characters")
+a = "python"
+for i in a:
+    print(i)
+print()
+
+# 4. Print the sum of numbers from 1 to 100
+print("Add")
+sum = 0
+for i in range(1,101):
+    sum+=i
+print(sum)
+print()
+
+# 5. Print multiplecation table of 5(like 5 x 1 = 5,... 5 x 10 = 50)
+for i in range(1,11):
+    print(f"5 x {i} = {5*i}")
+print()
+
+# 6. Print all elements
